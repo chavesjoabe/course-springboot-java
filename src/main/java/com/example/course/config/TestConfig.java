1e7e4065d32d9782b04ae4bc93bc41a11b90e62b
Joabe Chaves <chavesjoabe1@gmail.com>
2020-12-01 17:19:29 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -16,14 +19,25 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired //a anotacao autowired faz a injecao de dependencia
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
 		
 		User user1 = new User(0, "Jose das Couves", "jose@gmail.com", "555 0123" , "1234");
 		User user2 = new User(0, "Maria das Neves", "maria@gmail.com", "555 555" , "1234");
+		User user3 = new User(0, "Vitor Green", "vitor@gmail.com", "3444 555" , "1234");
+		User user4 = new User(0, "Ana Brown", "ana@gmail.com", "4444 555" , "1234");
 		
-		userRepository.saveAll(Arrays.asList(user1, user2)); //adciona os valores a uma lista
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user4);
+		
+		userRepository.saveAll(Arrays.asList(user1, user2, user3, user4)); //adciona os valores a uma lista
+		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 		
 	}
 	
--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,13 +1,20 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -19,6 +26,11 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User () {
 		
@@ -61,6 +73,11 @@ public class User implements Serializable{
 		return phone;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
@@ -94,6 +111,6 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
+
 	
 }
