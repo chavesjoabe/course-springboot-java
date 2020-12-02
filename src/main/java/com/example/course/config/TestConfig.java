7c6643c26e50113c21f5e4dd640640675647d09c
Joabe Chaves <chavesjoabe1@gmail.com>
2020-12-02 16:38:33 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.OrderRepository;
+import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,18 +41,42 @@ public class TestConfig implements CommandLineRunner{
 		User user2 = new User(0, "Maria das Neves", "maria@gmail.com", "555 555" , "1234");
 		User user3 = new User(0, "Vitor Green", "vitor@gmail.com", "3444 555" , "1234");
 		User user4 = new User(0, "Ana Brown", "ana@gmail.com", "4444 555" , "1234");
+	
+		userRepository.saveAll(Arrays.asList(user1, user2, user3, user4)); //adciona os valores a uma lista
 		
 		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.WAITING_PAYMENT, user1);
 		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, user2);
 		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.CANCELED, user4);
 		
+		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
+
 		Category category1 = new Category(null, "Electronics");
 		Category category2 = new Category(null, "Books");
 		Category category3 = new Category(null, "Computers");
 		
-		userRepository.saveAll(Arrays.asList(user1, user2, user3, user4)); //adciona os valores a uma lista
-		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+		
+		Product product1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product product2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product product3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		productRepository.saveAll(Arrays.asList(product1,product2,product3,product4,product5));
+		
+		product1.getCategories().add(category2);
+		product2.getCategories().add(category1);
+		product2.getCategories().add(category3);
+		product3.getCategories().add(category3);
+		product4.getCategories().add(category3);
+		product5.getCategories().add(category2);
+		
+		productRepository.saveAll(Arrays.asList(product1,product2,product3,product4,product5));//adiciona os produtos com as categorias
+		
+
+		
+		
+
 		
 	}
 	
--- src/main/java/com/example/course/entities/Category.java
+++ src/main/java/com/example/course/entities/Category.java
@@ -1,12 +1,18 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_category")
@@ -17,6 +23,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+	@JsonIgnore
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -47,6 +57,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 	
 
@@ -82,6 +96,10 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
+
+
+
+
 	
 	
 	
